61e0969b9b1a59ccdd54e77063be3503d64edf5c
Laura Vingada <laura@vingada.com>
2017-02-08 15:19:22 -0500

    added a new route

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-
+const {ObjectID} = require('mongodb');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
@@ -26,6 +26,29 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+// GET /todos/:id
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  // valid id using isValid
+  if (!ObjectID.isValid(id)) {
+    // 404 - send back empty body
+    return res.status(404).send('Invalid ID. Try again.');
+  }
+  // query findById and match the id
+  Todo.findById(id).then((todo) => {
+    // success
+    // if todo - send it back
+    if (todo) {
+      res.send({todo});
+    } else {
+      // if no todo - send back 404 with   empty body
+      // error
+        // 400 - empty send empty body
+      res.status(404).send('No todo with that id');
+    }
+  }).catch(e => res.status(400).send(`An error has occured`));
 });
 
 app.listen(3000, () => {
